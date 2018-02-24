488fbef38facbd87185e673ffbff4d53ffdbca81
Jared Wyce <wycejared@gmail.com>
2018-02-24 12:20:15 -0500

    minor optimizations

--- rubik/Solver.java
+++ rubik/Solver.java
@@ -17,7 +17,7 @@ public class Solver {
 	 * (the diameter of a 2x2x2 state graph is 14 if half twists count as 2 moves, 11 otherwise)
 	 */
 	public static String shortestPath(RubikState state) {
-		HashMap<RubikState, String> parents = bfs_visit(new RubikState(), state);
+		HashMap<RubikState, String> parents = bfs_visit(state, new RubikState());
 		String solutionPath = parents.get(state);
 		RubikState next = new RubikState(state.positions);
 		next.execute_move_seq(solutionPath);
@@ -34,22 +34,20 @@ public class Solver {
 	private static HashMap<RubikState, String> bfs_visit(RubikState source, RubikState destination) {
 		HashMap<RubikState, String> parents = new HashMap<>();
 		Queue<RubikState> queue = new LinkedList<RubikState>();
-		parents.put(source, null);
-		queue.add(source);
+		parents.put(destination, null);
+		queue.add(destination);
 		
-		while (!queue.isEmpty()) {
-			source = queue.remove();
-			for (Entry<String, RubikState> move : source.getReachableStates().entrySet()) {
+		while (true) {
+			destination = queue.remove();
+			for (Entry<String, RubikState> move : destination.getReachableStates().entrySet()) {
 				if (!parents.containsKey(move.getValue())) {
-					parents.put(move.getValue(), move.getKey());
+					parents.put(move.getValue(), move.getKey()); 
 					queue.add(move.getValue());
 				}
-				if (move.getValue().equals(destination))
+				if (move.getValue().equals(source))
 					return parents;
 			}
 		}
-		
-		return null;
 	}
 
 }
